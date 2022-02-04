f6ba4f941cc872ce6ee5c8b5f533d2141093d07f
GinoGreen <luigiverde97@gmail.com>
2022-02-04 16:24:30 +0100

    categories added in index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -23,6 +23,13 @@
                   <tr>
                      <th scope="row">{{ $post->id }}</th>
                      <td>{{ $post->title }}</td>
+                     <td>
+                        @if ($post->category)
+                           {{ $post->category->name }}
+                        @else
+                           -
+                        @endif
+                     </td>
                      <td class="d-flex justify-content-around">
                         <button class="btn btn-primary"><a class="text-dark" href="{{ route('admin.post.show', $post) }}">Show</a></button>
                         <button class="btn btn-success"><a class="text-white" href="{{ route('admin.post.edit', $post) }}">Edit</a></button>
@@ -38,6 +45,18 @@
          </table>
          {{ $posts->links() }}
       </div>
+      @foreach ($categories as $category)
+         <div>
+            <h1>{{ $category->name }}</h1>
+            <ul>
+               @forelse ($category->posts as $post)
+                  <li><a href="{{ route('admin.post.show', $post) }}">{{ $post->title }}</a></li>
+               @empty
+                  <p>Nessun post per la categoria <strong>{{ $category->name }}</strong></p>
+               @endforelse
+            </ul>
+         </div>
+      @endforeach
    </div>
 @endsection
 
