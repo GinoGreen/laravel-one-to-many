b1ded363341b17bcfa0cad84f2552b9f56bd9697
GinoGreen <luigiverde97@gmail.com>
2022-02-04 16:08:28 +0100

    table relationship

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,10 @@ class Post extends Model
 {
     protected $fillable = ['title', 'content', 'slug', 'category_id'];
 
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
+
     public static function generateUniqueSlug($string) {
         $slug = Str::slug($string, '-');
         $originalSlug = $slug;
